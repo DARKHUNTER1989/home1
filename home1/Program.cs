4054716ddbd3c1f70f35d03edbac5145cf13ce30
АНДРЕЙ <@DARKHUNTER>
2022-08-31 16:22:09 +0300

    Добавьте файлы проекта.

---
+++ home1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int waltingTime = 10;
+            string condicion = "Dead";
+            float speed = 5.4f;
+            double armor = 10.1;
+            bool work = true;
+            sbyte clickCount = -1;
+            char alert = '!';
+            uint blackList = 3000;
+            ulong allMoney = 80000000000;
+            byte age = 18;
+                  
+        }   
+    }
+}
